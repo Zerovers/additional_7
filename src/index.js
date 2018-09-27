2dd14f71f04aad1f730cf501e5b0fdd10d2c69b4
Zerovers <Zeromon19941994@mail.ru>
2018-09-27 22:08:39 +0300

    emplemented solveSudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    solve(matrix);
+    console.log(matrix);
+    return matrix;
+    
+}
+
+function unknowElements(matrix, mass) {
+    for (let i = mass[0]; i < 9; i++) {
+        for (let j = mass[1]; j < 9; j++) {
+            if (matrix[i][j] === 0) {
+                mass[0] = i;
+                mass[1] = j;
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+function rowContent(matrix, r, num) {
+    for (let i = 0; i < 9; i++) {
+        if (matrix[r][i] === num) {
+            return 1;
+        }
+    }
+    return ;
+}
+
+function colContent(matrix, c, num) {
+    for (let i = 0; i < 9; i++) {
+        if (matrix[i][c] === num) {
+            return 1;
+        }
+    }
+    return ;
+}
+
+function sectContent(matrix, r, c, num) {
+    let massR = (Math.floor(r / 3)) * 3;
+    let massC = (Math.floor(c / 3)) * 3;
+    for (let i = massR; i < massR + 3; i++)
+        for (let j = massC; j < massC + 3; j++) {
+            if (matrix[i][j] === num) {
+                return 1;
+            }
+        }
+    return ;
+}
+
+function solve(matrix) {
+    let mass = [0, 0];
+    if (!unknowElements(matrix, mass)) {
+        return 1;
+    }
+    let r = mass[0];
+    let c = mass[1];
+    for (let i = 1; i <= 9; i++) {
+        if (!rowContent(matrix, r, i) && !colContent(matrix, c, i) && !sectContent(matrix, r, c, i)) {
+            matrix[r][c] = i;
+            if (solve(matrix)) {
+                return 1;
+            }
+            matrix[r][c] = 0;
+        }
+    }
+    return ;
 }
